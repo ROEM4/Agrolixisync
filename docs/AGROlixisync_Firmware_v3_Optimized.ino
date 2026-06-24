ca02a5c5aa75fb0cb9319c72f70af7ac420fe297
ROEM4 <robertlopezgil83@gmail.com>
2026-06-24 17:44:24 -0500

    the-project.v4

--- docs/AGROlixisync_Firmware_v3_Optimized.ino
+++ docs/AGROlixisync_Firmware_v3_Optimized.ino
@@ -13,7 +13,7 @@ U8G2_SH1106_128X64_NONAME_F_HW_I2C display(U8G2_R0, U8X8_PIN_NONE);
 // ================= WIFI =================
 const char* ssid     = "COMPUTRONICA";
 const char* password = "computronic@";
-const char* serverUrl = "http://192.168.1.2:8000/api/sensor/data";  // ← IP del servidor Laravel
+const char* serverUrl = "http://192.168.1.9:8000/api/sensor/data";  // ← IP del servidor Laravel
 
 // ================= RTC =================
 RTC_DS3231 rtc;
